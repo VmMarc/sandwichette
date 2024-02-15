10b291b79918c026d0bff12689ac01dd30e443ba
flohrel <flohrel@student.42.fr>
2024-02-15 13:25:13 +0100

    build: add purgeCss and critical css
    - `npm run critical` generate an index with critical css and
    inline it
    - now build purge css file from unused css

--- gulp/index.js
+++ gulp/index.js
@@ -1,24 +1,22 @@
-// import { stream as critical } from 'critical';
 import gulp from 'gulp';
 import cached from 'gulp-cached';
 import csso from 'gulp-csso';
 import htmlmin from 'gulp-htmlmin';
 import GulpInlineSource from 'gulp-inline-source';
 import jshintPlugin from 'gulp-jshint';
+import gulpPurgeCss from 'gulp-purgecss';
+import rename from 'gulp-rename';
 import size from 'gulp-size';
 import useref from 'gulp-useref';
 import { exec } from 'node:child_process';
 import { rm } from 'node:fs/promises';
-import {
-  checkFaviconUpdate,
-  generateFavicon,
-  injectFaviconMarkups,
-} from './favicon.js';
+import { generateFavicon, injectFaviconMarkups } from './favicon.js';
 import image from './image.js';
 
 const jshint = jshintPlugin;
 const inlinesource = GulpInlineSource;
 const cache = cached;
+const purgecss = gulpPurgeCss;
 
 export async function clean() {
   await rm('../build', { recursive: true, force: true });
@@ -53,7 +51,7 @@ export function htmlCompress() {
         removeEmptyAttributes: true,
       }),
     )
-    .pipe(gulp.dest('../dist/'))
+    .pipe(gulp.dest('../build/'))
     .pipe(size());
 }
 
@@ -79,7 +77,33 @@ function copyCss() {
     .pipe(size());
 }
 
-export { checkFaviconUpdate, generateFavicon, injectFaviconMarkups };
+function purgeCss() {
+  return gulp
+    .src('../src/css/fontawesome/*.css')
+    .pipe(
+      purgecss({
+        content: ['../src/index.html'],
+      }),
+    )
+    .pipe(gulp.dest('../build/css/'));
+}
+
+function rejectedCss() {
+  return gulp
+    .src('../src/css/fontawesome/*.css')
+    .pipe(
+      rename({
+        suffix: '.rejected',
+      }),
+    )
+    .pipe(
+      purgecss({
+        content: ['../src/index.html'],
+        rejected: true,
+      }),
+    )
+    .pipe(gulp.dest('../build/css/'));
+}
 
 export const build = gulp.series(
   clean,
@@ -89,8 +113,12 @@ export const build = gulp.series(
     gulp.series(
       gulp.parallel(globalCssGen, jsLint),
       gulp.series(generateFavicon, injectFaviconMarkups),
-      gulp.series(htmlBuild, htmlCompress),
-      copyCss,
+      gulp.series(
+        gulp.parallel(purgeCss, rejectedCss),
+        htmlBuild,
+        htmlCompress,
+        copyCss,
+      ),
     ),
     image,
   ),
@@ -101,7 +129,15 @@ export default function () {
     ['../src/input.css', '../tailwind.config.js', '../src/index.html'],
     gulp.series(globalCssGen, gulp.series(htmlBuild, htmlCompress), copyCss),
   );
-  gulp.watch('../src/css/*.css', gulp.series(htmlBuild, htmlCompress, copyCss));
+  gulp.watch(
+    '../src/css/*.css',
+    gulp.series(
+      gulp.parallel(purgeCss, rejectedCss),
+      htmlBuild,
+      htmlCompress,
+      copyCss,
+    ),
+  );
   gulp.watch('../src/js/*.js', gulp.series(jsLint, htmlBuild, htmlCompress));
   gulp.watch(
     '../src/img/icon.png',
