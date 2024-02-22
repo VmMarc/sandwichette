28bbcac56b2e8283f46b665fe1a05aadaeb021c1
flohrel <flohrel@student.42.fr>
2024-02-22 18:02:30 +0100

    fix(menu-burger): fix resize problems

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,14 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 function burgerOpen() {
-  document.getElementById('header').style.backgroundPosition = 'right';
-  document.getElementById('burger-open').style.display = 'none';
-  document.getElementById('burger-close').style.display = 'block';
+  document.getElementById('header').classList.add('nav-menu');
+  document.getElementById('nav-menu__open').classList.add('nav-menu');
+  document.getElementById('nav-menu__close').classList.add('nav-menu');
   document.getElementById('instagram-icon').classList.add('btn-secondary');
 }
 // eslint-disable-next-line no-unused-vars
 function burgerClose() {
-  document.getElementById('header').style.backgroundPosition = 'left';
-  document.getElementById('burger-open').style.display = 'block';
-  document.getElementById('burger-close').style.display = 'none';
+  document.getElementById('header').classList.remove('nav-menu');
+  document.getElementById('nav-menu__open').classList.remove('nav-menu');
+  document.getElementById('nav-menu__close').classList.remove('nav-menu');
   document.getElementById('instagram-icon').classList.remove('btn-secondary');
 }
