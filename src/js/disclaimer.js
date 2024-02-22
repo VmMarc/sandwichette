8c4feddd855d6b8aedd588359c5a0bdf141a51a6
flohrel <flohrel@student.42.fr>
2024-02-22 14:54:10 +0100

    chore: rename functions + clean links

---
+++ src/js/disclaimer.js
@@ -0,0 +1,9 @@
+function disclaimerOpen(e) {
+  // e.preventDefault();
+  document.getElementById('overlay').style.display = 'block';
+}
+
+function disclaimerClose(e) {
+  // e.preventDefault();
+  document.getElementById('overlay').style.display = 'none';
+}
