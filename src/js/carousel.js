425cbd1b624f4739eae12705afb1601cc6d6ec99
bilaz666 <victor.me.marc@gmail.com>
2024-02-23 03:32:41 +0100

    fixed section-magazines position, added scrollLeft for testing in carousel.js

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,54 +1,30 @@
-// let magazines = document.getElementById('carousel').children;
-// console.log('magazines : ', magazines);
+let magazines = document.getElementById('carousel').children;
+let carousel = document.getElementById('carousel');
+console.log('magazines', magazines);
 
-// let position = 0;
+let position = 0;
 
-// function slider() {
-//   magazines[position].classList.add('z-0');
-//   console.log('magazine[position] : ', magazines[position]);
+console.log('position start : ', position);
 
-//   let zIndex = 0;
+// function slider() {  };
 
-//   for (let i = position + 1; i < magazines.length; i++) {
-//     console.log('loop 1 position : ', position);
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
 
-//     zIndex = zIndex + 10;
-//     console.log('loop 1 zIndex : ', zIndex);
+next.addEventListener('click', () => {
+  console.log('next');
+  position = position + 1 < magazines.length ? position + 1 : position;
+  carousel.scrollLeft += 240;
 
-//     magazines[i].classList.add(`z-[-${zIndex}]`);
-//     console.log('loop 1 magazines[i] : ', magazines[i]);
-//   }
+  console.log('position after next : ', position);
+  // slider();
+});
 
-//   zIndex = zIndex = 10;
+prev.addEventListener('click', () => {
+  console.log('prev');
+  position = position - 1 >= 0 ? position - 1 : position;
+  carousel.scrollLeft -= 240;
 
-//   for (let j = position - 1; j >= 0; j--) {
-//     console.log('loop 2 position : ', position);
-
-//     zIndex = zIndex + 10;
-//     console.log('loop 2 zIndex : ', zIndex);
-
-//     magazines[j].classList.add(`z-[-${zIndex}]`);
-//     console.log('loop 2 magazines[i] : ', magazines[j]);
-//   }
-// }
-
-// slider();
-
-// let nextButton = document.getElementById('next');
-// let prevButton = document.getElementById('prev');
-
-// nextButton.addEventListener('click', function () {
-//   console.log('next');
-//   position = position - 1 < magazines.length ? position + 1 : position;
-//   console.log('position next : ', position);
-//   slider();
-//   console.log('position after slider() next : ', position);
-// });
-
-// prevButton.addEventListener('click', function () {
-//   console.log('prev');
-//   position = position - 1 >= 0 ? position - 1 : position;
-//   console.log('position prev : ', position);
-//   slider();
-//   console.log('position after prev : ', position);
-// });
+  console.log('position after prev : ', position);
+  // slider();
+});
