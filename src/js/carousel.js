54500335430749887161c68a226bf7ad11919e2c
bilaz666 <victor.me.marc@gmail.com>
2024-02-23 03:32:41 +0100

    fixed section-magazines position, added scrollLeft for testing in carousel.js

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,43 +1,30 @@
 let magazines = document.getElementById('carousel').children;
-console.log('magazines', magazines); //TODO
-
 let carousel = document.getElementById('carousel');
-console.log('carousel', carousel); //TODO
-console.log('magazines[0].classList[2] : ', magazines[0].classList[2]); //TODO
+console.log('magazines', magazines);
+
+let position = 0;
+
+console.log('position start : ', position);
+
+// function slider() {  };
 
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 
-let position = 0;
-console.log('position start : ', position); //TODO
-
-// FONCTION
-function Slider() {
-  for (let i = 0; i < magazines.length; i++) {
-    magazines[i].classList.remove(magazines[i].classList[3]); //Todo change logic
-    console.log('magazines[i] : ', magazines[i]); //TODO
-
-    let translateX = position * 100;
-    magazines[i].classList.add(`translate-x-[-${translateX}%]`);
-  }
-}
-
-// NEXT
 next.addEventListener('click', () => {
-  console.log('next'); //TODO
-
+  console.log('next');
   position = position + 1 < magazines.length ? position + 1 : position;
-  Slider();
+  carousel.scrollLeft += 240;
 
-  console.log('position after next : ', position); //TODO
+  console.log('position after next : ', position);
+  // slider();
 });
 
-// PREV
 prev.addEventListener('click', () => {
-  console.log('prev'); //TODO
-
+  console.log('prev');
   position = position - 1 >= 0 ? position - 1 : position;
-  Slider();
+  carousel.scrollLeft -= 240;
 
-  console.log('position after prev : ', position); //TODO
+  console.log('position after prev : ', position);
+  // slider();
 });
