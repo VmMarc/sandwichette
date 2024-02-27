01af3c4cb0491c4aaa78b3e9e41022e70d8f0048
bilaz666 <victor.me.marc@gmail.com>
2024-02-27 16:54:41 +0100

    added function slider to carousel.js

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,30 +1,43 @@
 let magazines = document.getElementById('carousel').children;
+console.log('magazines', magazines); //TODO
+
 let carousel = document.getElementById('carousel');
-console.log('magazines', magazines);
-
-let position = 0;
-
-console.log('position start : ', position);
-
-// function slider() {  };
+console.log('carousel', carousel); //TODO
+console.log('magazines[0].classList[2] : ', magazines[0].classList[2]); //TODO
 
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 
-next.addEventListener('click', () => {
-  console.log('next');
-  position = position + 1 < magazines.length ? position + 1 : position;
-  carousel.scrollLeft += 240;
+let position = 0;
+console.log('position start : ', position); //TODO
 
-  console.log('position after next : ', position);
-  // slider();
+// FONCTION
+function Slider() {
+  for (let i = 0; i < magazines.length; i++) {
+    magazines[i].classList.remove(magazines[i].classList[3]); //Todo change logic
+    console.log('magazines[i] : ', magazines[i]); //TODO
+
+    let translateX = position * 100;
+    magazines[i].classList.add(`translate-x-[-${translateX}%]`);
+  }
+}
+
+// NEXT
+next.addEventListener('click', () => {
+  console.log('next'); //TODO
+
+  position = position + 1 < magazines.length ? position + 1 : position;
+  Slider();
+
+  console.log('position after next : ', position); //TODO
 });
 
+// PREV
 prev.addEventListener('click', () => {
-  console.log('prev');
-  position = position - 1 >= 0 ? position - 1 : position;
-  carousel.scrollLeft -= 240;
+  console.log('prev'); //TODO
 
-  console.log('position after prev : ', position);
-  // slider();
+  position = position - 1 >= 0 ? position - 1 : position;
+  Slider();
+
+  console.log('position after prev : ', position); //TODO
 });
