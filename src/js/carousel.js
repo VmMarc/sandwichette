376d51e7529764ca7072388283f7c843ea3a00c2
bilaz666 <victor.me.marc@gmail.com>
2024-02-28 18:22:42 +0100

    added: images scaling in slider fonction

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,42 +1,32 @@
 let magazines = document.getElementById('carousel-content').children;
-console.log('magazines', magazines); //TODO
-
-let carousel = document.getElementById('carousel');
-console.log('carousel', carousel); //TODO
 
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 
 let position = 0;
-console.log('position start : ', position); //TODO
 
-// FONCTION
+// FONCTION SLIDER
 function Slider() {
   for (let i = 0; i < magazines.length; i++) {
+    let scaleValue = i === position ? 'scale(1)' : 'scale(.75)';
     let translateValue = position * 240;
     magazines[i].setAttribute(
       'style',
-      `transform: translateX(-${translateValue}px)`,
+      `transform: translateX(-${translateValue}px) ${scaleValue}`,
     );
   }
 }
 
-// NEXT
-next.addEventListener('click', () => {
-  console.log('next'); //TODO
+Slider();
 
+// EVENT NEXT
+next.addEventListener('click', () => {
   position = position + 1 < magazines.length ? position + 1 : position;
   Slider();
-
-  console.log('position after next : ', position); //TODO
 });
 
-// PREV
+// EVENT PREV
 prev.addEventListener('click', () => {
-  console.log('prev'); //TODO
-
   position = position - 1 >= 0 ? position - 1 : position;
   Slider();
-
-  console.log('position after prev : ', position); //TODO
 });
