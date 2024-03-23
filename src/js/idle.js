0721b7d7bc1fb7fae613a12451be1e35786037a1
flohrel <flohrel@student.42.fr>
2024-03-23 15:04:58 +0100

    fix: new CSP policy, add idle event listened

--- src/js/idle.js
+++ src/js/idle.js
@@ -106,12 +106,16 @@ function clearIntervalAndTimeOut() {
 hideIdleImages();
 idleImages();
 
-document.addEventListener('mousemove', () => {
+function resetTimer() {
   clearIntervalAndTimeOut();
   hideIdleImages();
-});
+}
 
-document.addEventListener('scroll', () => {
-  clearIntervalAndTimeOut();
-  hideIdleImages();
-});
+window.addEventListener('load', resetTimer, true);
+window.addEventListener('mousemove', resetTimer, true);
+window.addEventListener('mousedown', resetTimer, true);
+window.addEventListener('touchstart', resetTimer, true);
+window.addEventListener('touchmove', resetTimer, true);
+window.addEventListener('click', resetTimer, true);
+window.addEventListener('keydown', resetTimer, true);
+window.addEventListener('scroll', resetTimer, true);
